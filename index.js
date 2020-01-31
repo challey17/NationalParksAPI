2a513df597446f1f1c335cd7c0c1f99ba92a932e
Chayce Knaub <chknaub@yahoo.com>
2020-01-31 02:44:52 -0500

    stuckpoint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // put your own value below!
 const apiKey = "H4ICqlHFH8sNsOGrkkMIyIK9sNkUAQl3sFPdjV03";
-const searchURL = "https://developer.nps.gov/api/v1";
+const searchURL = "https://developer.nps.gov/api/v1/parks";
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params).map(
@@ -16,32 +16,35 @@ function displayResults(responseJson) {
   console.log(responseJson);
   $("#results-list").empty();
   // iterate through the items array
-  for (let i = 0; i < responseJson.items.length; i++) {
+  for (let i = 0; i < responseJson.data.length; i++) {
     // for each video object in the items
     //array, add a list item to the results
     //list with the video title, description,
     //and thumbnail
+
     $("#results-list").append(
-      `<li><h3>${responseJson.items[i].snippet.title}</h3>
-      <p>${responseJson.items[i].snippet.description}</p>
-      <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+      `<li><h3>${responseJson.data[i].fullname}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <h4> Park Location: ${responseJson.data[i].addresses.physical}
+      <a hfref='${responseJson.data[i].url}'>
       </li>`
     );
   }
+
   //display the results section
   $("#results").removeClass("hidden");
+  console.log("displayResults running ok");
 }
 
-function getYouTubeVideos(query, maxResults = 10) {
+function getParks(query, maxResults) {
   const params = {
-    key: apiKey,
-    q: query,
-    part: "snippet",
-    maxResults,
-    type: "video"
+    //key: apiKey,
+    stateCode: query,
+
+    limit: maxResults
   };
   const queryString = formatQueryParams(params);
-  const url = searchURL + "?" + queryString;
+  const url = searchURL + "?" + queryString + "&api_key=" + apiKey;
 
   console.log(url);
 
@@ -59,11 +62,12 @@ function getYouTubeVideos(query, maxResults = 10) {
 }
 
 function watchForm() {
+  console.log("watchForm running ok");
   $("form").submit(event => {
     event.preventDefault();
     const searchTerm = $("#js-search-term").val();
     const maxResults = $("#js-max-results").val();
-    getYouTubeVideos(searchTerm, maxResults);
+    getParks(searchTerm, maxResults);
   });
 }
 
